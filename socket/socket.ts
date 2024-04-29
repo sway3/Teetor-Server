c9756e8ca3f117f5b5e4914ed8a8ab8a93c7560f
Seungwoo Kim <svvoo31o@gmail.com>
2024-04-29 12:46:27 +0200

    Feat: error

--- socket/socket.ts
+++ socket/socket.ts
@@ -21,7 +21,7 @@ app.use(userRoutes);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://teetor-client.vercel.app",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     credentials: true,
   },
