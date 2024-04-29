3159d927de9b34a82a420a6c6fd3a2ddc882ce82
Seungwoo Kim <svvoo31o@gmail.com>
2024-04-29 11:33:07 +0200

    Feat: socket

--- socket/socket.ts
+++ socket/socket.ts
@@ -4,35 +4,13 @@ import express from "express";
 import userRoutes from "../routes/userRoutes";
 import { getUserId } from "../utils/authFunctions";
 import cookieParser from "cookie-parser";
-
-const cors = require("cors");
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-const corsOptions = {
-  origin: "https://teetor-client.vercel.app",
-  credentials: true,
-  methods: ["GET", "POST", "HEAD", "PUT", "PATCH", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-app.use(cors(corsOptions));
-
-app.options("*", (req, res) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://teetor-client.vercel.app"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.status(200).end();
-});
-
+app.use(cors());
 app.use(userRoutes);
 
 const server = http.createServer(app);
